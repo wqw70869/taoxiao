d8327d41026a7c6efa5f8e49afef190f574f50b2
wqw70869 <42890219+wqw70869@users.noreply.github.com>
2018-12-21 21:25:38 +0800

    Add files via upload

--- controller/CollectionController.java
+++ controller/CollectionController.java
@@ -1,11 +1,14 @@
 package com.example.taoxiao;
 
+import com.alibaba.fastjson.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.util.Date;
 import java.util.List;
 
@@ -14,14 +17,24 @@ import java.util.List;
 public class CollectionController {
 	@Autowired
 	private CollectionMapper cmapper;
-	
+	@Autowired
+	private GoodsMapper goodms;
 	
 	@RequestMapping("/getcollection")
 	public String getcollection(String UserId){
-		JSONObject result = new JSONObject();
 	    List<Collection> str=cmapper.getcollection(UserId);
-	    result.put("collection",str);//添加返回的字段
-		return result.toJSONString();
+		JSONArray json = new JSONArray();
+		for(Collection t : str){
+            List<sellgoods> str1=goodms.select2(t.getGoodsId());
+            JSONObject jo = new JSONObject();
+			jo.put("goodid", str1.get(0).getGoodid());
+			jo.put("goodname", str1.get(0).getGoodname());
+			jo.put("price", str1.get(0).getPrice());
+			jo.put("description",str1.get(0).getDescription());
+			jo.put("userid",str1.get(0).getSellid());
+		    json.add(jo);
+		}
+		return json.toJSONString();
 	}
 	@RequestMapping("/setcollection")
 	public String setcollection(String UserId, String GoodsId, Date Uptime)
--- controller/GoodsController.java
+++ controller/GoodsController.java
@@ -1,8 +1,12 @@
 package com.example.taoxiao;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +21,7 @@ public class GoodsController {
 		@Autowired
 		private GoodsMapper goodsm;
 		
-		@RequestMapping("/newgoods")
+		@RequestMapping("/newgoods")//发布商品
 		public String addgoods(String goodsname,String sellid, String kinds,double price, String description, Date uptime){
 			JSONObject result = new JSONObject();
 			if( goodsm.insert(goodsname,sellid,kinds,price,description,uptime))
@@ -26,24 +30,52 @@ public class GoodsController {
 				result.put("status", "false");
 			return result.toJSONString();
 		}
-		@RequestMapping("/getgoodsINF")
-		public String select2(String id){//根据商品ID查找商品
-			JSONObject result = new JSONObject();
-			List<sellgoods> str=goodsm.select2(id);
-			result.put("sreach",str);
-			return result.toJSONString();
-
+	@RequestMapping("/newgoods")
+	public String insertphoto(Integer goodid)
+	{
+		JSONObject result = new JSONObject();
+		String name=goodid+".png";
+		if(	goodsm.insertphoto(goodid,name)) {
+			result.put("status", "true");
 		}
-		@RequestMapping("/getgoodsINFBytype")//根据类型返回商品信息
-		public String select3(String type){
+		else {
+			result.put("status", "false");
+		}
+		return result.toJSONString();
+	}
+	@RequestMapping("/goodshome")
+	public String home()
+	{
+		String title;
 
-			JSONObject result = new JSONObject();
-			List<sellgoods> str=goodsm.select3(type);
-			result.put("sreach",str);
-			return result.toJSONString();
+		JSONObject result = new JSONObject();
+		List<sellgoods> str=goodsm.select1();
+		for(int i = 0; i<str.size(); i++) {
+			str.get(i).getGoodname();
+		}
+		result.put("home",str);
+		return result.toJSONString();
+	}
+		@RequestMapping("/getgoodsINF")
+		public String select2(Integer id){//根据商品ID查找商品
+			JSONArray json = new JSONArray();
+			List<sellgoods> str=goodsm.select2(id);
+			for(sellgoods t:str) {
+				JSONObject jo = new JSONObject();
+				jo.put("goodid", str.get(0).getGoodid());
+				jo.put("goodname", str.get(0).getGoodname());
+				jo.put("price", str.get(0).getPrice());
+				jo.put("description", str.get(0).getDescription());
+				jo.put("userid", str.get(0).getSellid());
+				jo.put("uptime",str.get(0).getUptime());
+				jo.put("type",str.get(0).getType());
+				json.add(jo);
+			}
+			return json.toJSONString();
+
 		}
 		@RequestMapping("/delete")//删除商品信息
-			public String delete(String id){
+			public String delete(Integer id){
 			JSONObject result = new JSONObject();
 			if( goodsm.delete(id))
 				result.put("status", "true");
@@ -63,18 +95,73 @@ public class GoodsController {
 		@RequestMapping("/Sreachsell")//搜索买卖的商品
 				public String Searchsell(String Text)
 		{
-			JSONObject result = new JSONObject();
+
 			List<sellgoods> str=goodsm.Searchsell(Text);
-			result.put("sreach",str);
-			return result.toJSONString();
+			JSONArray json = new JSONArray();
+			for(sellgoods t:str) {
+				JSONObject jo = new JSONObject();
+				jo.put("goodid", str.get(0).getGoodid());
+				jo.put("goodname", str.get(0).getGoodname());
+				jo.put("price", str.get(0).getPrice());
+				jo.put("description", str.get(0).getDescription());
+				jo.put("userid", str.get(0).getSellid());
+				jo.put("uptime",str.get(0).getUptime());
+				jo.put("type",str.get(0).getType());
+				json.add(jo);
+			}
+			return json.toJSONString();
+		}
+		public String Searchsell(String type,String Text)
+		{
+
+			List<sellgoods> str=goodsm.Searchsell(type,Text);
+			JSONArray json = new JSONArray();
+			for(sellgoods t:str) {
+				JSONObject jo = new JSONObject();
+				jo.put("goodid", str.get(0).getGoodid());
+				jo.put("goodname", str.get(0).getGoodname());
+				jo.put("price", str.get(0).getPrice());
+				jo.put("description", str.get(0).getDescription());
+				jo.put("userid", str.get(0).getSellid());
+				jo.put("uptime",str.get(0).getUptime());
+				jo.put("type",str.get(0).getType());
+				json.add(jo);
+			}
+			return json.toJSONString();
 		}
 	@RequestMapping("/Sreachrent")//搜索租借的商品
 	public String Searchrent(String Text)
 	{
-		JSONObject result = new JSONObject();
+
 		List<rentgoods> str=goodsm.Searchrent(Text);
-		result.put("sreach",str);
-		return result.toJSONString();
+		JSONArray json = new JSONArray();
+		for(rentgoods t:str) {
+			JSONObject jo = new JSONObject();
+			jo.put("goodid", str.get(0).getGoodid());
+			jo.put("goodname", str.get(0).getGoodname());
+			jo.put("description", str.get(0).getdescription());
+			json.add(jo);
+		}
+		return json.toJSONString();
+	}
+	@RequestMapping("/Mysellgoods")//查看自己的发布
+	public String Mysellgoods(Integer sellid)
+	{
+
+		List<sellgoods> str=goodsm.Mysellgoods(sellid);
+		JSONArray json = new JSONArray();
+		for(sellgoods t:str) {
+			JSONObject jo = new JSONObject();
+			jo.put("goodid", str.get(0).getGoodid());
+			jo.put("goodname", str.get(0).getGoodname());
+			jo.put("price", str.get(0).getPrice());
+			jo.put("description", str.get(0).getDescription());
+			jo.put("userid", str.get(0).getSellid());
+			jo.put("uptime",str.get(0).getUptime());
+			jo.put("type",str.get(0).getType());
+			json.add(jo);
+		}
+		return json.toJSONString();
 	}
 
 }
